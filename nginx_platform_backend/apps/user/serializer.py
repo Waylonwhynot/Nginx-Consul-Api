a7b56b83902ef42a14101a747c13f4f29ed56433
gtdong <michaeldong@126.com>
2021-08-14 16:39:03 +0800

    fdsgs

--- nginx_platform_backend/apps/user/serializer.py
+++ nginx_platform_backend/apps/user/serializer.py
@@ -1,7 +1,5 @@
 from .models import UserProfile,Permission,Role,Organization
 from rest_framework import serializers
-from rest_framework import exceptions
-from rest_framework_jwt.serializers import jwt_payload_handler, jwt_encode_handler
 class CommonModelSerializer(serializers.ModelSerializer):
     create_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M', required=False, read_only=True)
     update_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M', required=False, read_only=True)
